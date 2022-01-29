c38b76e026e292c69cceada0474f0656351ca11a
André Cassiano <andre.p.cassiano@accenture.com>
2022-01-28 22:34:49 -0300

    alguns teste de código.

--- primeiraAplicacao/src/App.java
+++ primeiraAplicacao/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int y = 34;
+        double x = 45.0000;
+
+        System.out.println("Olá Mundo "+ y);
+
+        System.out.print("Resultado do calculo é " + x );
     }
 }
